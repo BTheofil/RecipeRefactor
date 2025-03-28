5f516823bb1e48659640ee62f1bd7c0ea80c878b
BTheofil <btheofil7@gmail.com>
2025-03-28 19:44:15 +0100

    created core module for colors and theme

---
+++ core/src/main/java/hu/tb/core/ui/theme/Type.kt
@@ -0,0 +1,6 @@
+package hu.tb.core.ui.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
+
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,3 +21,4 @@ dependencyResolutionManagement {
 
 rootProject.name = "RecipeRefactor"
 include(":app")
+include(":core")
